98769d04b7a447ca63a9ab9b22ff57443608c1ca
Аликина Ольга <oalikina@eset.local>
2021-06-24 11:22:06 +0300

    features

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 const popup = document.querySelector('.popup')
 const popupMenu = document.querySelector('.popup__menu')
-const featuresTitle = document.querySelector('.features__desc')
+const featuresTitle = document.querySelector('.features__bottom-title')
 const featuresText = document.querySelector('.features__bottom-text')
-const higingArrow = document.querySelectorAll('.hiring__arrow')
-const hiringFront = document.querySelector('.hiring__block-group_front')
-const hiringCommunity = document.querySelector('.hiring__block-group_community')
-const hiringDesigner = document.querySelector('.hiring__block-group_designer')
+const hiringFront = document.querySelector('.hiring__block_front')
+const hiringCommunity = document.querySelector('.hiring__block_community')
+const hiringDesigner = document.querySelector('.hiring__block_designer')
 const hiringFrontText = document.querySelector('.hiring__popup-text_front')
 const hiringCommunityText = document.querySelector('.hiring__popup-text_community')
 const hiringDesignerText = document.querySelector('.hiring__popup-text_designer')
+const burger = document.querySelector('.burger')
+const arrowFront = hiringFront.querySelector('.hiring__block-link_front')
+const arrowCommunity = hiringCommunity.querySelector('.hiring__block-link_community')
+const arrowDesigner = hiringDesigner.querySelector('.hiring__block-link_designer')
 
-const openFrontText = () => hiringFrontText.classList.toggle('active');
-const openDesignerText = () => hiringDesignerText.classList.toggle('active');
-const openCommunityText = () => hiringCommunityText.classList.toggle('active');
+function openHeaderNav() {
+  const openPopup = () => popupMenu.classList.toggle('popup__menu_active')
+  const openBurger = () => popupMenu.classList.toggle('popup__menu_active');
+  popup.addEventListener('click', openPopup)
+  burger.addEventListener('click', openBurger)
+}
+openHeaderNav()
+
+function openHiringInfo() {
+  function openFrontText() {
+    hiringFrontText.classList.toggle('hiring__popup-text_active');
+    arrowFront.classList.toggle('hiring__block-link_active'); hiringFront.classList.toggle('hiring__block_active')
+  }
+  function openDesignerText() {
+    hiringDesignerText.classList.toggle('hiring__popup-text_active');
+    arrowDesigner.classList.toggle('hiring__block-link_active'); hiringDesigner.classList.toggle('hiring__block_active')
+  }
+  function openCommunityText() {
+    hiringCommunityText.classList.toggle('hiring__popup-text_active');
+    arrowCommunity.classList.toggle('hiring__block-link_active'); hiringCommunity.classList.toggle('hiring__block_active')
+  }
+  hiringFront.addEventListener('click', openFrontText)
+  hiringDesigner.addEventListener('click', openDesignerText)
+  hiringCommunity.addEventListener('click', openCommunityText)
+}
+openHiringInfo()
+
+function openFeaturesInfo() {
+  const toggleFeaturesText = () => featuresText.classList.toggle('features_active')
+  featuresTitle.addEventListener('click', toggleFeaturesText)
+}
+openFeaturesInfo()
 
 let slideIndex = 1;
 showSlides(slideIndex);
@@ -37,18 +69,3 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " slider__btn_active";
 }
-
-
-const openFeaturesText = () => featuresText.classList.toggle('active')
-const openPopup = () => popupMenu.classList.toggle('popup_active')
-
-popup.addEventListener('click', openPopup)
-featuresTitle.addEventListener('click', openFeaturesText)
-hiringFront.addEventListener('click', openFrontText)
-hiringDesigner.addEventListener('click', openDesignerText)
-hiringCommunity.addEventListener('click', openCommunityText)
-
-
-
-
-
